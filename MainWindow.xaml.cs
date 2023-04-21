5925708a84a3232f0350dcb78574c23063cae090
PixelBest <nick.poroschin@yandex.ru>
2023-04-21 16:30:39 +0500

    2

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,117 +21,185 @@ using System.Xml.Linq;
 using System.Collections.ObjectModel;
 using static System.Net.Mime.MediaTypeNames;
 using System.Security.Cryptography;
+using System.Windows.Media.Animation;
+using System.Text.RegularExpressions;
 
 namespace Practicheskaya_6
 {
     public partial class MainWindow : Window
     {
-        string path = Directory.GetCurrentDirectory();
+        ObservableCollection<Department> depList = new ObservableCollection<Department>();
+        ObservableCollection<Employee> emp = new ObservableCollection<Employee>();
+        int check = 1;
+        
         public MainWindow()
         {
             InitializeComponent();
         }
+        public void CreateEmployee()
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < 9; i++)
+            {
+                Department dep = new Department();
+                for (int j = 0; j < 100; j++)
+                {
+                    int a = rnd.Next(1, 4);
+                    if (a == 1)
+                    {
+                        dep.employees.Add(new Intern("Интерн" + rnd.Next(1, 500).ToString()));
+                        emp.Add(new Intern { Age = dep.employees[j].Age, ID = dep.employees[j].ID, Name = dep.employees[j].Name, Position = dep.employees[j].Position, Salary = dep.employees[j].Salary });
+                    }
+                    else if (a == 2)
+                    {
+                        dep.employees.Add(new Manager("Менеджер" + rnd.Next(1, 500).ToString()));
+                        emp.Add(new Manager { Age = dep.employees[j].Age, ID = dep.employees[j].ID, Name = dep.employees[j].Name, Position = dep.employees[j].Position, Salary = dep.employees[j].Salary });
+                    }
+                        
+                    else if (a == 3)
+                    {
+                        dep.employees.Add(new Admin("Менеджер" + rnd.Next(1, 500).ToString()));
+                        emp.Add(new Admin { Age = dep.employees[j].Age, ID = dep.employees[j].ID, Name = dep.employees[j].Name, Position = dep.employees[j].Position, Salary = dep.employees[j].Salary });
+                    }
+                    check++;
+                }
+                depList.Add(dep);
+            }
+        }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            List<Employee> employee = new List<Employee>();//создание работников
-            for(int i = 0; i < 200; i++)
+            CreateEmployee();
+            
+            dataGrid1.ItemsSource = emp;    
+        }
+        private void dataGrid1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+        private void treeView1_Initialized(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            TreeViewItem bestHacker = new TreeViewItem();
+            TreeViewItem dep1 = new TreeViewItem();
+            TreeViewItem dep2 = new TreeViewItem();
+            TreeViewItem dep3 = new TreeViewItem();
+            int d1 = rnd.Next(1, 5);
+            int d2 = rnd.Next(1, 5);
+            int d3;
+            int check = 4;
+
+            treeView1.Items.Add(bestHacker);
+            bestHacker.Header = "Лучшие программисты";
+            bestHacker.Items.Add(dep1);
+            bestHacker.Items.Add(dep2);
+            bestHacker.Items.Add(dep3);
+            bestHacker.FontSize = 14;
+
+            dep1.Header = "dep1";
+            for (int i = 0; i < d1; i++)
             {
-                employee.Add(new Employee(i + 1));
+                dep1.Items.Add("dep" + check);
+                check++;
             }
 
-            XmlSerializer xml = new XmlSerializer(typeof(List<Employee>));//указание типа
-
-            FileStream fs = new FileStream($@"{path}/depart1.xml", FileMode.OpenOrCreate);//указание пути до xml файла
-            xml.Serialize(fs, employee);//сериализация
-            fs.Close();
-
-            MessageBox.Show("Файл xml создан", "Успех", MessageBoxButton.OK);
-        }
-
-        private void doubleClick1(object sender, RoutedEventArgs e) { DepartNum(4); }//вызов класса по поиску нужных работников двойным кликом
-        private void doubleClick2(object sender, RoutedEventArgs e) { DepartNum(5); }
-        private void doubleClick3(object sender, RoutedEventArgs e) { DepartNum(6); }
-        private void doubleClick4(object sender, RoutedEventArgs e) { DepartNum(7); }
-        private void doubleClick5(object sender, RoutedEventArgs e) { DepartNum(8); }
-        private void doubleClick6(object sender, RoutedEventArgs e) { DepartNum(9); }
-        private void doubleClick7(object sender, RoutedEventArgs e) { DepartNum(10); }
-        private void doubleClick8(object sender, RoutedEventArgs e) { DepartNum(11); }
-        private void doubleClick9(object sender, RoutedEventArgs e) { DepartNum(12); }
-        private void DepartNum(int a)
-        {
-            XmlSerializer xml = new XmlSerializer(typeof(Employee[]));//Указание типа
-            FileStream fs = new FileStream($@"{path}/depart1.xml", FileMode.OpenOrCreate);//указание пути до xml файла
-
-            Employee[] emp1 = xml.Deserialize(fs) as Employee[];//создание массива для всех работников
-            List<Employee> test = new List<Employee>();
-            foreach (Employee emp in emp1)//запись всех работников в List
+            dep2.Header = "dep2";
+            for (int i = 0; i < d2; i++)
             {
-                if (emp.NumDep == a)//проверка нужного отдела
-                    test.Add(emp);//добавление каждого работника в List
+                dep2.Items.Add("dep" + check);
+                check++;
             }
-            dataGrid1.ItemsSource = test;//вывод данных в dataGried
-            fs.Close();//закрытие файлового потока
+
+            dep3.Header = "dep3";
+            d3 = 9 - (d1 + d2);
+            for (int i = 0; i < d3; i++)
+            {
+                dep3.Items.Add("dep" + check);
+                check++;
+            }
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)//выводит работников с нужным номером отдела
+        private void treeView1_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
-            XmlSerializer xml = new XmlSerializer(typeof(List<Employee>));//Указание типа
-            FileStream fs = new FileStream($@"{path}/depart1.xml", FileMode.OpenOrCreate);//указание пути до xml файла
-
-            List<Employee> emp1 = xml.Deserialize(fs) as List<Employee>;//создание массива для всех работников
-            List<Employee> test = new List<Employee>();
-            foreach (Employee emp in emp1)//запись всех работников в List
-                    test.Add(emp);//добавление каждого работника в List
-            dataGrid1.ItemsSource = test;//вывод данных в dataGried
-            fs.Close();//закрытие файлового потока
+            for (int i = 0; i < depList.Count; i++)
+            {
+                if (depList[i].Dep == treeView1.SelectedItem.ToString())
+                    dataGrid1.ItemsSource = depList[i].employees;
+            }
         }
 
-        private void RaiseSalary_Click(object sender, RoutedEventArgs e)
+        private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            Window1 w1 = new Window1();
-            w1.ShowDialog();
+            dataGrid1.ItemsSource = emp;
         }
     }
-    public class Employee//создание класса работника
+    public abstract class Employee//создание класса работника
     {
         static Random rnd = new Random();
-        public int ID { get; set; }
+        public float ID { get; set; }
         public string Name { get; set; }
         public int Age { get; set; }
-        public string Position { get; set; }
-        public int NumDep { get; set; }
-        public int Salary { get; set; }
-        public Employee() { }
-        public Employee(string name, int age, string position, int numDep, int salary)
+        public abstract string Position { get; set; }
+        public abstract int Salary { get; set; }
+        public static float count = 0;
+        public Employee(string name, int age)
         {
-            if (name == "1")
-                Name = "Начальник Начальниковов Начальникович" + rnd.Next(1,1000);
-            else if (name == "2")
-                Name = "Работник Работников Работникович" + rnd.Next(1, 1000);
-            else if(name == "3")
-                Name = "Интерн Интернов Интернович" + rnd.Next(1, 1000);
-
+            ID = ++count;
+            count = count - 0.5f;
+            Name = name;
             Age = age;
-
-            if (name == "1")
-                Position = "Nachalnik";
-            else if (name == "2")
-                Position = "Employee";
-            else if(name=="3")
-                Position = "Intern";
-
-            NumDep = numDep;
-
-            if (name == "1")
-                Salary = rnd.Next(3000, 5000);
-            else if (name == "3")
-                Salary = 500;
-            else
-                Salary = salary;
         }
-        public Employee(int id) : this((string)Convert.ToString(rnd.Next(1, 4)), (int)rnd.Next(20, 60), (string)Convert.ToString(rnd.Next(1,4)), (int)rnd.Next(4,13), (int)rnd.Next(1300, 2000))
+        public Employee() : this("", 0) { }
+        public Employee(string name) : this(name, (int)rnd.Next(20, 60)) { }
+    }
+    public class Department
+    {
+        static Random rnd = new Random();
+        public string Dep { get; set; }
+        public int ID { get; set; }
+        public ObservableCollection<Employee> employees { get; set; }
+        public static int count1 = 4;
+        public Department(string dep)
         {
-            ID = id;
+            Dep = dep;
+            employees = new ObservableCollection<Employee>();
+            ID = count1++;
         }
+        public Department() : this($"dep{count1}") { }
+    }
+    public class Intern : Employee
+    {
+        static Random rnd = new Random();
+        public override int Salary { get; set; }
+        public override string Position { get; set; }
+        public Intern(string name) : base(name, (int)rnd.Next(20, 60))
+        {
+            Salary = 500;
+            Position = "Intern";
+        }
+        public Intern() : base("", 0) { }
+    }
+    public class Manager : Employee
+    {
+        static Random rnd = new Random();
+        public override int Salary { get; set; }
+        public override string Position { get; set; }
+        public Manager(string name) : base(name, (int)rnd.Next(20, 60))
+        {
+            Salary = 2500;
+            Position = "Manager";
+        }
+        public Manager() : base("", 0) { }
+    }
+    public class Admin : Employee
+    {
+        static Random rnd = new Random();
+        public override int Salary { get; set; }
+        public override string Position { get; set; }
+        public Admin(string name) : base(name, (int)rnd.Next(20, 60))
+        {
+            Salary = 5000;
+            Position = "Admin";
+        }
+        public Admin() : base("", 0) { }
     }
 }
